3194ac9c847130112f1348bc1ee60077a5a53095
Piotr Gaszczynski <piotrek.gaszczynski@gmail.com>
2024-01-10 18:56:05 +0100

    Fix bugs with floats and strings

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+"""Warehouse mamager
+Author: Pedro van Code
+Last update: 10.01.2024
+Any comments welcome :)
+"""
+
 import csv
 import logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s', filename="warehouse_manager.log")
@@ -9,7 +15,7 @@ def main():
     print_info()
     load()
     while True:
-        user_decision = get_user_input("What would you like to do? exit/show/add/sell/revenue/save/load: ")  
+        user_decision = get_user_input_string("What would you like to do? exit/show/add/sell/revenue/save/load: ")  
         logging.info(f"User decision {user_decision}")
         if user_decision == 'exit':
             exit_program()
@@ -35,11 +41,28 @@ def print_info():
     print("--- warehouse_manager.py ---")
     
     
-def get_user_input(question):
+def get_user_input_string(question):
     """Get input string from user and returns lower and stripped."""
-    user_input_string = input(question)
-    logging.info(f"User input string: {user_input_string}")
-    return user_input_string.lower().strip() if isinstance(user_input_string, str) else int(user_input_string)
+    while True:
+        user_input = input(question)
+        logging.info(f"User input: {user_input}")
+        if is_string(user_input):
+            return user_input.lower().strip()
+        else:
+            print("Wrong input. Not a string. Try again.")
+            logging.info(f"{user_input} not a string.")
+
+
+def get_user_input_float(question):
+    """Get input from user and returns as a float."""
+    while True:
+        user_input = input(question)
+        logging.info(f"User input: {user_input}")
+        if not is_string(user_input):
+            return convert_input_to_float(user_input)
+        else:
+            print("Wrong input. Lookas like string. Try again.")
+            logging.info(f"{user_input} is a string.")
 
 
 def wrong_input():
@@ -64,25 +87,50 @@ def create_item(name, quantity, unit, unit_price):
         
 def add_items():
     print("Adding to warehouse...")
-    new_item = create_item(get_user_input("New item name: "),
-                         int(get_user_input("New item quantity: ")), 
-                         get_user_input("New item unit: "),
-                         int(get_user_input("New item unit price: "))
+    new_item = create_item(get_user_input_string("New item name: "),
+                         get_user_input_float("New item quantity: "), 
+                         get_user_input_string("New item unit: "),
+                         get_user_input_float("New item unit price: ")
                          )
     logging.info(f"New item to add: {new_item}")
     items.append(new_item)
     logging.info("New item added")
     
     
+def convert_input_to_float(input):
+    """Converts entered decimals with ',' to floats with '.' if possible."""
+    if ',' in input:
+        splitted = input.split(',')
+        return float(splitted[0]+'.'+splitted[1]) if len(splitted) == 2 else input
+    else:
+        return float(input)
+    
+    
+def is_string(input):
+    """Checks if input is float (returns False), int (returns False) or string (returns True)."""
+    try:
+        input = float(input)
+        logging.info(f"{input} is float")
+        return False
+    except ValueError:
+        try:
+            input = int(input)
+            logging.info(f"{input} is integer")
+            return False
+        except ValueError:
+            logging.error(f"{input} is string")
+            return True
+        
+    
 def sell_items():
     print("Sell from warehouse...")
-    sell_item_name = get_user_input("Item to sell: ")
-    sell_item_quantity = int(get_user_input("Quantity to sell: "))
+    sell_item_name = get_user_input_string("Item to sell: ")
+    sell_item_quantity = get_user_input_float("Quantity to sell: ")
     logging.info(f"Item to sell: {sell_item_name} quantity {sell_item_quantity}")
     for item in items:
         if item['name'] == sell_item_name:
-            if item['quantity'] >= sell_item_quantity:
-                item['quantity'] = item['quantity'] - sell_item_quantity
+            if float(item['quantity']) >= sell_item_quantity:
+                item['quantity'] = float(item['quantity']) - sell_item_quantity
                 print(f"Succesfully sold {sell_item_quantity} {item['unit']} of {sell_item_name}")
                 sold_item = create_item(sell_item_name,
                                       sell_item_quantity,
@@ -101,7 +149,7 @@ def sell_items():
 
 
 def calculate_value_of_products(array):
-    value = [item['unit_price'] * item['quantity'] for item in array]
+    value = [float(item['unit_price']) * float(item['quantity']) for item in array]
     return sum(value)
 
 
