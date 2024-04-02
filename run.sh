8c50431fdba69f4d612bce6e37139a2e985c04c5
Piotr Gaszczynski <piotrek.gaszczynski@gmail.com>
2024-04-02 03:56:56 +0200

    refactor

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 [ -d "$HOME/.local/bin" ] && PATH="$HOME/.local/bin:$PATH"
 export PATH
 export FLASK_ENV=development
-flask run --debug --port 5000 --host=192.168.50.181
+flask run --debug --port 5000
